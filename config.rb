1721fb744087f94cdcdab6b5f278a2c43340dcc8
Vladimir Shevchuk <dosandkv@gmail.com>
2014-12-09 22:34:19 +0200

    add project-popup styling finished;

--- config.rb
+++ config.rb
@@ -29,3 +29,30 @@ line_comments = false
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+# Remove the random string in the sprite filename
+# http://www.codechewing.com/library/automatically-generate-css-sprites-with-sass/
+# If you don’t want the random string that’s appended to the filename when
+# a new sprite is created, here’s how to remove it. You will need to open config.rb
+# inside your Compass project, and add the following code:
+
+# Make a copy of sprites with a name that has no uniqueness of the hash.
+on_sprite_saved do |filename|
+  if File.exists?(filename)
+    FileUtils.cp filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
+    # Note: Compass outputs both with and without random hash images.
+    # To not keep the one with hash, add: (Thanks to RaphaelDDL for this)
+    FileUtils.rm_rf(filename)
+  end
+end
+
+# Replace in stylesheets generated references to sprites
+# by their counterparts without the hash uniqueness.
+on_stylesheet_saved do |filename|
+  if File.exists?(filename)
+    css = File.read filename
+    File.open(filename, 'w+') do |f|
+      f << css.gsub(%r{-s[a-z0-9]{10}\.png}, '.png')
+    end
+  end
+end
